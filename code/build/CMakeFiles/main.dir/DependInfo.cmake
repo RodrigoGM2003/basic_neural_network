05f4fa92bb7c19cd55f15b8e9fd30822e22c409f
rodrigogm <rodrigogm@correo.ugr.es>
2024-11-30 22:42:30 +0100

    Adapting the code

---
+++ code/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/xxdri/Desktop/Universidad/Proyectos personales/RN/StandardNeuronalNetwork/code/src/data_set.cpp" "CMakeFiles/main.dir/src/data_set.cpp.obj" "gcc" "CMakeFiles/main.dir/src/data_set.cpp.obj.d"
+  "C:/Users/xxdri/Desktop/Universidad/Proyectos personales/RN/StandardNeuronalNetwork/code/src/functions.cpp" "CMakeFiles/main.dir/src/functions.cpp.obj" "gcc" "CMakeFiles/main.dir/src/functions.cpp.obj.d"
+  "C:/Users/xxdri/Desktop/Universidad/Proyectos personales/RN/StandardNeuronalNetwork/code/src/layer.cpp" "CMakeFiles/main.dir/src/layer.cpp.obj" "gcc" "CMakeFiles/main.dir/src/layer.cpp.obj.d"
+  "C:/Users/xxdri/Desktop/Universidad/Proyectos personales/RN/StandardNeuronalNetwork/code/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.obj" "gcc" "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "C:/Users/xxdri/Desktop/Universidad/Proyectos personales/RN/StandardNeuronalNetwork/code/src/n_network.cpp" "CMakeFiles/main.dir/src/n_network.cpp.obj" "gcc" "CMakeFiles/main.dir/src/n_network.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "C:/Users/xxdri/Desktop/Universidad/Proyectos personales/RN/StandardNeuronalNetwork/code/bin/main.exe"
+  "C:/Users/xxdri/Desktop/Universidad/Proyectos personales/RN/StandardNeuronalNetwork/code/bin/main.exe.manifest"
+  "C:/Users/xxdri/Desktop/Universidad/Proyectos personales/RN/StandardNeuronalNetwork/code/bin/main.pdb"
+  "CMakeFiles/main.dir/src/data_set.cpp.obj"
+  "CMakeFiles/main.dir/src/data_set.cpp.obj.d"
+  "CMakeFiles/main.dir/src/functions.cpp.obj"
+  "CMakeFiles/main.dir/src/functions.cpp.obj.d"
+  "CMakeFiles/main.dir/src/layer.cpp.obj"
+  "CMakeFiles/main.dir/src/layer.cpp.obj.d"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "CMakeFiles/main.dir/src/n_network.cpp.obj"
+  "CMakeFiles/main.dir/src/n_network.cpp.obj.d"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
